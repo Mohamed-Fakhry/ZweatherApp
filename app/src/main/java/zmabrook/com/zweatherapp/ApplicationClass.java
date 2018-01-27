f1a76c499f0ab31cc01a8307fcd5bf0faa05b000
zmabrook <Hh2512011>
2018-01-27 21:31:40 +0200

    Bug Fixes. application class added Multidexing added

---
+++ app/src/main/java/zmabrook/com/zweatherapp/ApplicationClass.java
@@ -0,0 +1,15 @@
+package zmabrook.com.zweatherapp;
+
+import android.app.Application;
+
+
+/**
+ * Created by zMabrook on 27/01/18.
+ */
+
+public class ApplicationClass extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
